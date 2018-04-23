e2e8d02475068f58cb3c7264ea7b0f91060683d0
Antonio Campos <camposantonio949@gmail.com>
2018-04-23 18:43:17 -0500

    creacion de nuevas vistas y tabs

--- app/src/main/java/com/example/antoniocampos/etominv/inicioSesion.java
+++ app/src/main/java/com/example/antoniocampos/etominv/inicioSesion.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class inicioSesion extends AppCompatActivity {
 TextView h;
+Button iniciar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +21,15 @@ TextView h;
             public void onClick(View v) {
                 Intent s = new Intent(inicioSesion.this, rescatarPin.class);
                 startActivity(s);
+            }
+        });
+
+        iniciar = (Button) findViewById(R.id.iniciar);
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(inicioSesion.this, tablas.class);
+                startActivity(in);
             }
         });
     }
